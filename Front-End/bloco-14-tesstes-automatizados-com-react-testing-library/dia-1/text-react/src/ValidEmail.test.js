3f9d2df037c27324a615a60580ca390fe7f37f31
Alexandre <alexmanson12345@gmail.com>
2022-08-12 13:13:42 -0300

    verificação de testes com RTL

---
+++ Front-End/bloco-14-tesstes-automatizados-com-react-testing-library/dia-1/text-react/src/ValidEmail.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ValidEmail from './ValidEmail';
+
+describe('Verificação de email', () => {
+
+  test('Testando um componente, caso o email seja válido.', () => {
+    const EMAIL_USER = 'email@email.com';
+    render(<ValidEmail email={ EMAIL_USER } />);
+    const isValid = screen.getByText('Email Válido');
+    expect(isValid).toBeInTheDocument();
+  });
+
+  test('Testando um componente, caso o email seja inválido.', () => {
+    const EMAIL_USER = 'emailerrado';
+    render(<ValidEmail email={ EMAIL_USER } />);
+    const isValid = screen.getByText('Email Inválido');
+    expect(isValid).toBeInTheDocument();
+  });  
+});
